5e3ea3be6f8b0987ac89090f6fd025aa592d9bcb
蔡镇鸿 <1229542068@qq.com>
2021-05-07 16:53:19 +0800

    feat:增加用户信息模块

---
+++ src/views/user/children/catalog/Catalog.vue
@@ -0,0 +1,23 @@
+ <!--
+ * @desc: 文章操作及信息
+ * @author czh996.top
+ * @Date: 2021年4月7日
+ * @props: [Obejct] ations - 用于渲染操作列表的数据
+ * @props: [Boolean] shareBtnShow - 控制分享按钮隐藏显示
+ * @example
+ *  <article-item-comment-button :actions="actions" :shareBtnShow="shareBtnShow"></article-item-comment-button>
+ -->
+<template>
+  <div>文章</div>
+</template>
+
+<script>
+export default {
+  name: '',
+};
+</script>
+
+<style scoped lang='scss'>
+@import '@app';
+@import '@theme';
+</style>
