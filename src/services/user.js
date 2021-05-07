173f78609639eaabe52705be788e28f4376729e2
蔡镇鸿 <1229542068@qq.com>
2021-05-07 17:20:55 +0800

    feat:增加登录注册 api

--- src/services/user.js
+++ src/services/user.js
@@ -10,10 +10,23 @@ export function getUser(params) {
     })
 }
 
-export function sendEmail(data) {
+/** 点击注册，向邮箱发送邮件
+ * @param {String} email - 邮箱
+ * @param {String} password - 密码
+ * @return 
+ */
+export function post_email_signUp(email, password) {
     return server({
         method: "POST",
-        url: "/post/sendEmail",
-        data
+        url: "/post/email/sign-up",
+        data: { email, password }
+    })
+}
+
+export function post_user_login(email, password) {
+    return server({
+        method: "POST",
+        url: '/post/user/login',
+        data: { email, password }
     })
 }
