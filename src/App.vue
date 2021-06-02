ad46e8ca8461f965b7c039d6c49130bdc196e265
蔡镇鸿 <1229542068@qq.com>
2021-06-02 21:03:56 +0800

    feat:添加信息提示

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,58 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <base-notice :show="msgShow" @hidden="hidden">
+      <div :class="msgType | msgIcon">{{ msgText }}</div>
+    </base-notice>
   </div>
 </template>
 
 <script>
-import { createNamespacedHelpers } from 'vuex';
-const { mapActions } = createNamespacedHelpers('user');
+import { BaseNotice } from '@/components/base';
 
 export default {
-  methods: {
-    ...mapActions(['getUserInfo']),
+  components: {
+    BaseNotice,
   },
 
-  created: function () {
-    // 获取用户信息
-    let id = window.location.pathname;
-    let reg = /(setting|\/)/g;
-    if (reg.test(id)) {
-      id = '/';
-    }
+  data: function () {
+    return {
+      msgShow: false,
+    };
+  },
 
-    this.getUserInfo(id);
+  computed: {
+    msgType: function () {
+      return this.$store.state.msgBox.type;
+    },
+    msgText: function () {
+      return this.$store.state.msgBox.text;
+    },
+    flag: function () {
+      return Boolean(this.$store.state.msgBox.flag);
+    },
+  },
+
+  watch: {
+    flag: function () {
+      this.msgShow = true;
+    },
+  },
+
+  methods: {
+    hidden: function () {
+      this.msgShow = false;
+    },
+  },
+
+  filters: {
+    msgIcon: function (v) {
+      switch (v) {
+        case 'succeed': {
+          return 'icon__succeed';
+        }
+      }
+    },
   },
 };
 </script> 
