02798063e34847bdce285be7bae84f8deec200c3
caizhenhong <1229542068@qq.com>
2021-09-25 16:01:05 +0800

    feat:添加MyButton组件置灰状态

--- ui/src/components/Base/MyButton.vue
+++ ui/src/components/Base/MyButton.vue
@@ -1,14 +1,14 @@
 <template>
-  <div button @click="clickEmit" class="flex flex-items-center flex-center theme f14 button" :class="propStyle"><slot></slot></div>
+  <div :button='propStyle[3].disabled?null:true' @click="clickEmit" class="unselected flex flex-items-center flex-center theme f14 button" :class="propStyle"><slot></slot></div>
 </template>
 
 <script>
 export default {
-  props: ['primary', 'large', 'big'],
+  props: ['primary', 'large', 'big', 'disabled'],
 
   computed:{
     propStyle:function(){
-      return [{primary:this.primary != undefined}, {large:this.large != undefined}, {big:this.big != undefined}]
+      return [{primary:this.primary != undefined}, {large:this.large != undefined}, {big:this.big != undefined},{disabled:this.disabled === true}]
     }
   },
 
@@ -35,5 +35,21 @@ export default {
 .big {
   padding: 12px 20px;
   border-radius: 6px;
+}
+.disabled {
+  position: relative;
+  border-color: #ffffffff;
+  pointer-events: none;
+  &::after{
+    content: '';
+    display: block;
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background: rgba(223, 223, 223, 0.404);
+  }
+
 }
 </style>
