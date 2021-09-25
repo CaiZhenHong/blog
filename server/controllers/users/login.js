0aed7333631f1a5a9934ef182eaa689951140543
caizhenhong <1229542068@qq.com>
2021-09-25 16:00:37 +0800

    feat:完成登录功能

--- server/controllers/users/login.js
+++ server/controllers/users/login.js
@@ -1,8 +1,21 @@
 const errorModel = require('../../model/errorModel')
 const succeedModel = require('../../model/succeedModel')
-module.exports = function(ctx, next){
+const {getAccount} = require('../../db/users/index')
+module.exports = async(ctx, next) => {
     const { count, password } = ctx.request.body
-    ctx.session.user = count
-    ctx.status = 200
-    ctx.body = succeedModel({})
+    let body = succeedModel({})
+    if(!count || !password){ 
+        body = errorModel(200400,'参数错误');
+    }else{
+        const doc = await getAccount(count)
+        if(!doc) {
+            body = errorModel(200404, '账户不存在'); 
+        }else if(doc && doc.password != password){
+            body = errorModel(200413, '密码错误')
+        }else{
+            ctx.session.account = count
+        }
+    }
+    ctx.body = body
+    await next()
 }
--- ui/src/view/login/Login.vue
+++ ui/src/view/login/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pt100">
+  <div class="pt100" @keydown="onKeydown">
     <div class="h550 w400 flex flex-column flex-items-center pl35 pr35 bg-white_1 bd center ">
       <div button class="flex flex-column flex-items-center" @click="toHome">
         <div  class="mt35"><img class="h64 w64" src="@/assets/image/icon/logo-theme.png" alt=""></div>
@@ -11,7 +11,7 @@
         <MyInput icon='&#xe604;' class="mt16" type='password' v-model="password" placeholder='密码' :verifyError='Boolean(passwordError)' />
         <div class="mt10 mb10 f12 red_1 h20">{{ userNameError||passwordError||''}}</div>
       </div>
-      <my-button primary big class="fit-width" @click="loginHandle">登录</my-button>
+      <my-button primary big class="fit-width" @click="loginHandle" :disabled='!userName||!password||loading'><MyLoadIcon v-if="loading" nontext /><span v-else>登录</span></my-button>
       <div class="flex flex-center f14 theme_1 mt120">
         <div button>找回密码</div>
         <span class="ml5 mr5">|</span>
@@ -26,15 +26,24 @@ import { checkUserName, checkPassword } from '@/util/verification'
 
 export default {
   data: function(){
-    return { userName: 'asd',  password: 'dsadsads', userNameError: '', passwordError: ''}
+    return { userName: 'asd',  password: 'dsadsads', userNameError: '', passwordError: '', loading: false}
   },
   methods: {
     toHome: function() { this.$router.push({name: 'HomeArticleNew'}) },
+
     loginHandle: function() {
+      this.loading = true
       this.userNameError = checkUserName(this.userName)
       !this.userNameError && (this.passwordError = checkPassword(this.password))
-      if(this.passwordError) return
-      this.$store.dispatch('user/login',{count:this.userName, password:this.password})
+      if(this.passwordError || this.userNameError) return this.loading = false
+      this.$store.dispatch('user/login',{count:this.userName, password:this.password}).finally(()=>{
+        this.loading = false
+      })
+    },
+
+    onKeydown: function(e) {
+      const key = window.event.keyCode ? window.event.keyCode : window.event.which
+      key == 13 &&(this.userName && this.password && !this.loading && !this.dialogopen) && (this.loginHandle()) 
     }
   },
 }
