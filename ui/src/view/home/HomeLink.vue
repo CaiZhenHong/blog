416f4cf67037d88187443d814bd67b8876ec1279
caizhenhong <1229542068@qq.com>
2021-09-25 16:58:54 +0800

    feat：增加空白显示

--- ui/src/view/home/HomeLink.vue
+++ ui/src/view/home/HomeLink.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page">
     <div class="f14 bdb pt10 pb10 pl20 pr20 black_2">我的链接</div>
-    <ul class="mt20 flex flex-start flex-wrap">
+    <ul v-if="icons && icons.length" class="mt20 flex flex-start flex-wrap">
       <li  class="flex flex-column flex-items-center icon  mb20" v-for="({ name, url }, index) in icons" :key="index">
         <my-pop>
         <div button slot="trigger" iconfont class="w40 h40 bd" :class="[`icon_${name}`]" @click="to(url)"></div>
@@ -9,6 +9,10 @@
         </my-pop>
       </li>
     </ul>
+    <div class="fcenter flex-column mt20 mb20" v-else>
+      <div class="fit-width h100  empty"></div>
+      <div class="f16 gray_3">暂无链接</div>
+    </div>
   </div>
 </template>
 
